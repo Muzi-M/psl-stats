6b5e3c05ee602c4b34589d8da56ba49379ccb858
Muziwethu Mlotshwa <MUZIWETM@speciality.pepkor.co.za>
2025-06-25 09:54:33 +0200

    updated api route to define mongoDB properties

--- src/app/api/players/route.ts
+++ src/app/api/players/route.ts
@@ -3,17 +3,21 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   const season = parseInt(req.nextUrl.searchParams.get("season") || "2023");
+  const team = req.nextUrl.searchParams.get("team");
+
+  const query: any = { type: "player", season };
+  if (team) query.teamName = team;
 
   const client = await connectToDatabase();
   const db = client.connection.db;
+
   const players = await db
     .collection("psl")
-    .find({ type: "player", season })
+    .find(query)
     .project({
       player: 1,
       statistics: 1,
       teamName: 1,
-      season: 1,
       _id: 0,
     })
     .toArray();

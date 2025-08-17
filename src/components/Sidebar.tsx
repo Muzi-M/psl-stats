5931f0599f4f67f8bfe2c850f4839e6293d762c6
Muziwethu Mlotshwa <MUZIWETM@speciality.pepkor.co.za>
2025-08-17 19:07:31 +0200

    changed logo

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from "next/link";
-import { Card } from "./ui/card";
 import { Button } from "./ui/button";
 import { usePathname } from "next/navigation";
 import { useTheme } from "next-themes";
